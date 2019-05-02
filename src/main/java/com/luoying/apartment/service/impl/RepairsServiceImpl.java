ff1fc02579038f7e264e33d31a32b4e7f193468c
yjc <614183334@qq.com>
2019-05-02 20:31:25 +0800

    更新图片对比

--- src/main/java/com/luoying/apartment/service/impl/RepairsServiceImpl.java
+++ src/main/java/com/luoying/apartment/service/impl/RepairsServiceImpl.java
@@ -3,9 +3,16 @@ package com.luoying.apartment.service.impl;
 import com.luoying.apartment.base.bean.MyPage;
 import com.luoying.apartment.pojo.Repairs;
 import com.luoying.apartment.dao.RepairsMapper;
+import com.luoying.apartment.pojo.Student;
+import com.luoying.apartment.pojo.User;
 import com.luoying.apartment.service.IRepairsService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.luoying.apartment.service.IStudentService;
+import com.luoying.apartment.service.UserService;
+import com.luoying.apartment.utils.MyUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * <p>
@@ -17,6 +24,10 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class RepairsServiceImpl extends ServiceImpl<RepairsMapper, Repairs> implements IRepairsService {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private IStudentService studentService;
 
     @Override
     public MyPage<Repairs> getRepairsPage(MyPage<Repairs> page) {
@@ -29,5 +40,36 @@ public class RepairsServiceImpl extends ServiceImpl<RepairsMapper, Repairs> impl
     public Repairs getRepairsById(Long id) {
         Repairs repairs = this.baseMapper.queryRepairsById(id);
         return repairs;
+    }
+
+    @Override
+    @Transactional(rollbackFor = {Exception.class})
+    public void addRepairs(Repairs repairs, User user) {
+        MyUtil.checkNull(user);
+        MyUtil.checkNull(user.getId(),"用户为空");
+        //获取当前登录学生的user信息
+        User stu = userService.getById(user.getId());
+        MyUtil.checkNull(stu);
+        MyUtil.checkNull(stu.getStudentId());
+        Student student =  studentService.getById(user.getStudentId());
+        //填上报人信息
+        repairs.setSponsorId(student.getId());
+        repairs.setSponsorName(student.getStudentName());
+        repairs.setSponsorPhone(student.getPhone());
+        repairs.setDormitoryId(student.getDormitoryId());
+        this.baseMapper.insert(repairs);
+    }
+
+    @Override
+    @Transactional(rollbackFor = {Exception.class})
+    public void updateRepairs(Repairs repairs, User user) {
+        MyUtil.checkNull(user);
+        MyUtil.checkNull(user.getId(),"用户为空");
+        User manager = userService.getById(user.getId());
+        MyUtil.checkNull(manager);
+        MyUtil.check(manager.getUserType()==0, "账号类型非管理员");
+        repairs.setHandleUserId(manager.getId());
+        repairs.setStatus(1);
+        this.baseMapper.updateById(repairs);
     }
 }
